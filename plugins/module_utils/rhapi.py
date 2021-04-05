fbf7960781e4e1124c04a409633e26d4cd6d6ccf
Patrick Easters <peasters@redhat.com>
2021-04-05 06:59:28 -0400

    Add module

---
+++ plugins/module_utils/rhapi.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import json
+import os
+from ansible.module_utils.urls import fetch_url
+from ansible.module_utils._text import to_text
+from ansible.module_utils.basic import env_fallback
+from oauthlib.oauth2 import Client
+from requests_oauthlib import OAuth2Session
+
+class RedHatAPIClient:
+
+    def __init__(self, module):
+        self.module = module
+        self.base_url = os.getenv('RHSMAPI_BASE_URL', 'https://api.access.redhat.com/management/v1')
+        self.sso_token_url = os.getenv('RHSMAPI_TOKEN_URL', 'https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token')
+        self.timeout = 30
+        self.token = {
+            'refresh_token': module.params['refresh_token'],
+            'access_token': 'to_be_replaced',
+            'token_type': 'Bearer',
+            'expires_in': '-1',
+        }
+        extra = {'client_id': module.params['refresh_token_client']}
+        self.session = OAuth2Session(module.params['refresh_token_client'], token=self.token, auto_refresh_url=self.sso_token_url,
+                                     auto_refresh_kwargs=extra, token_updater=self._save_token)
+
+    def _build_url(self, path):
+        if path[0] == '/':
+            path = path[1:]
+        return '%s/%s' % (self.base_url, path)
+
+    def _save_token(self, token):
+        self.token=token
+
+    def request(self, method, path, data=None):
+        url = self._build_url(path)
+        return self.session.request(method, url, json=data, timeout=self.timeout)
+
+    def get(self, path, data=None):
+        return self.request('GET', path, data)
+
+    def put(self, path, data=None):
+        return self.request('PUT', path, data)
+
+    def post(self, path, data=None):
+        return self.request('POST', path, data)
+
+    def delete(self, path, data=None):
+        return self.request('DELETE', path, data)
---
+++ plugins/modules/cloud_provider_account.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = '''
+---
+module: cloud_provider_account
+short_description: Manages an RHSM Cloud Access provider account
+description: >
+  This module will ensure a cloud provider account exists in RHSM and optionally
+  verifies it for use with auto registration.
+options:
+  state:
+    description:
+      - Add or remove a cloud provider account
+    choices: [ present, absent ]
+    default: present
+    type: str
+  refresh_token:
+    description:
+      - Offline refresh token for authentication with Red Hat APIs
+      - Tokens can be generated at https://access.redhat.com/management/api
+    required: true
+    type: str
+  refresh_token_client:
+    description:
+      - Client ID used to generate the refresh token
+      - Typically 'rhsm-api' for tokens generated at access.redhat.com and 'cloud-services' for cloud.redhat.com
+    default: rhsm-api
+    type: str
+  provider:
+    description:
+      - Short Name of the cloud provider (AWS, MSAZ, etc)
+    required: true
+    type: str
+  id:
+    description:
+      - Account ID of the cloud provider account
+    required: true
+    type: strss
+  nickname:
+    description:
+      - Nickname or short description of the cloud provider account
+    type: str
+  verification_identity:
+    description:
+      - Cloud VM instance identity document
+      - Only needed to verify a cloud provider account for use with RHSM auto registration
+    type: str
+  verification_signature:
+    description:
+      - Cloud VM instance identity signature
+      - Only needed to verify a cloud provider account for use with RHSM auto registration
+    type: str
+author:
+    - Patrick Easters (@patrickeasters)
+'''
+
+EXAMPLES = '''
+'''
+
+RETURN = '''
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.redhatinsights.subscriptions.plugins.module_utils.rhapi import RedHatAPIClient
+import base64
+
+def return_changed(module):
+  result = dict(changed=True)
+  module.exit_json(**result)
+
+
+def run_module():
+    module_args = dict(
+        state=dict(choices=['present', 'absent'], default='present'),
+        refresh_token=dict(type='str', required=True),
+        refresh_token_client=dict(type='str', default='rhsm-api'),
+        provider=dict(type='str', required=True),
+        id=dict(type='str', required=True),
+        nickname=dict(type='str', required=False, default=''),
+        verification_identity=dict(type='str', required=False, default=''),
+        verification_signature=dict(type='str', required=False, default=''),
+    )
+
+    result = dict(
+        changed=False,
+    )
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    client = RedHatAPIClient(module)
+
+    # Get existing cloud providers
+    enabled = client.get('/cloud_access_providers/enabled')
+    enabled.raise_for_status()
+    providers = enabled.json()
+
+    # Find existing account in list of enabled providers
+    account = {}
+    for p in providers['body']:
+        if p['shortName'] == module.params['provider']:
+            for a in p['accounts']:
+                if a['id'] == module.params['id']:
+                    account = a
+                    break
+            break
+    
+    # Create account if missing
+    if not account:
+        if module.check_mode:
+            return_changed(module)
+        new_account = {
+            'id': module.params['id'],
+            'nickname': module.params['nickname'],
+        }
+        path = '/cloud_access_providers/'+module.params['provider']+'/accounts'
+        create = client.post(path, json=list(new_account))
+        create.raise_for_status()
+        result['changed'] = True
+    else:
+        # Update existing account if nickname is different
+        if module.params['nickname'] and module.params['nickname'] != account['nickname']:
+            if module.check_mode:
+                return_changed(module)
+            updated_account = {'nickname': module.params['nickname']}
+            path = '/cloud_access_providers/'+module.params['provider']+'/accounts/'+account['id']
+            update = client.put(path, data=updated_account)
+            update.raise_for_status()
+            result['changed'] = True
+    
+    # Submit an account verification if provided and account is not already verified
+    if module.params['verification_identity'] and 'verified' in account and not account['verified']:
+        if module.check_mode:
+            return_changed(module)
+        # The API expects identity and signature to be base64 encoded
+        # Because base64.b64encode returns values as bytes, we decode it back to a string
+        verification = {
+            'identity': base64.b64encode(module.params['verification_identity'].encode('utf-8')).decode('utf-8'),
+            'signature': base64.b64encode(module.params['verification_signature'].encode('utf-8')).decode('utf-8'),
+        }
+        path = '/cloud_access_providers/'+module.params['provider']+'/accounts/'+module.params['id']+'/verification'
+        verify = client.put(path, data=verification)
+        verify.raise_for_status()
+        result['changed'] = True
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
